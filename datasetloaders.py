bed16d5cad93242461c3b70d5c3e9c3d48dd802a
Francis Brochu <frank.brochu.2@gmail.com>
2019-03-15 15:20:12 -0400

    Confirmed source of food101 bug, cleared problematic augmentation

--- datasetloaders.py
+++ datasetloaders.py
@@ -57,10 +57,6 @@ def load_base_dataset(dataset_name, batch_size=32, num_workers=3):
             torchvision.transforms.RandomHorizontalFlip(0.5),
             torchvision.transforms.ColorJitter(brightness=0.25, contrast=0.25, saturation=0.25, hue=0.25),
             torchvision.transforms.RandomChoice([torchvision.transforms.Resize((224, 224)),
-                                                 # torchvision.transforms.Compose([
-                                                 #     torchvision.transforms.RandomRotation(45),
-                                                 #     torchvision.transforms.CenterCrop(224)
-                                                 # ]),
                                                  torchvision.transforms.RandomResizedCrop((224, 224),
                                                                                           scale=(0.25, 1.0)),
                                                  torchvision.transforms.RandomCrop(224, pad_if_needed=True)
