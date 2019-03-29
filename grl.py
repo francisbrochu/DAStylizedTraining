ba95e4e65ede3e159f6a0aad071c053d56cdc389
Francis Brochu <frank.brochu.2@gmail.com>
2019-03-29 15:25:12 -0400

    Added parameter to gradient reversal layer

--- grl.py
+++ grl.py
@@ -3,6 +3,11 @@ import torch.nn as nn
 
 class GradientReversalLayer(nn.Module):
 
+    def __init__(self, c = 1):
+        super(GradientReversalLayer, self).__init__()
+
+        self.c = -1 * c
+
     def forward(self, x):
-        h = x.register_hook(lambda grad: grad * -1)
+        h = x.register_hook(lambda grad: grad * self.c)
         return x
