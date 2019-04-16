Gradient reversal bug is fixed! Now checking if lambda regulatization of DA works as intended

daresnets.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision
-from grl import GradientReversalLayer, LambdaLayer, ReverseLayerF
+from grl import LambdaLayer, ReverseLayerF
 
 
 # for dog breed identification
@@ -15,7 +15,6 @@ class DBIDAResNet(nn.Module):
         input_fc_dim = self.model.fc.in_features
         self.model.fc = nn.Linear(input_fc_dim, 120)
 
-        #self.grl = GradientReversalLayer()
         self.domainfc = nn.Linear(input_fc_dim, 2)
         self.ll = LambdaLayer(lambda_param=lambda_param)
 
@@ -35,7 +34,6 @@ class DBIDAResNet(nn.Module):
 
         classif_output = self.model.fc(output)
 
-        # domain_output = self.grl(output)
         domain_output = ReverseLayerF.apply(output)
         domain_output = self.domainfc(domain_output)
         domain_output = self.ll(domain_output)
@@ -53,45 +51,6 @@ class DCDAResNet(nn.Module):
         input_fc_dim = self.model.fc.in_features
         self.model.fc = nn.Linear(input_fc_dim, 2)
 
-        # self.grl = GradientReversalLayer()
-        self.domainfc = nn.Linear(input_fc_dim, 2)
-        # self.ll = LambdaLayer(lambda_param=lambda_param)
-
-    def forward(self, x):
-        output = self.model.conv1(x)
-        output = self.model.bn1(output)
-        output = self.model.relu(output)
-        output = self.model.maxpool(output)
-
-        output = self.model.layer1(output)
-        output = self.model.layer2(output)
-        output = self.model.layer3(output)
-        output = self.model.layer4(output)
-
-        output = self.model.avgpool(output)
-        output = output.view(output.size(0), -1)
-
-        classif_output = self.model.fc(output)
-
-        # domain_output = self.grl(output)
-        domain_output = ReverseLayerF.apply(output)
-        domain_output = self.domainfc(domain_output)
-        # domain_output = self.ll(domain_output)
-
-        return classif_output, domain_output
-
-
-# for dice
-class DiceDAResNet(nn.Module):
-
-    def __init__(self, lambda_param=0.1):
-        super(DiceDAResNet, self).__init__()
-
-        self.model = torchvision.models.resnet34(pretrained=True)
-        input_fc_dim = self.model.fc.in_features
-        self.model.fc = nn.Linear(input_fc_dim, 6)
-
-        #self.grl = GradientReversalLayer()
         self.domainfc = nn.Linear(input_fc_dim, 2)
         self.ll = LambdaLayer(lambda_param=lambda_param)
 
@@ -111,7 +70,42 @@ class DiceDAResNet(nn.Module):
 
         classif_output = self.model.fc(output)
 
-        #domain_output = self.grl(output)
+        domain_output = ReverseLayerF.apply(output)
+        domain_output = self.domainfc(domain_output)
+        domain_output = self.ll(domain_output)
+
+        return classif_output, domain_output
+
+
+# for dice
+class DiceDAResNet(nn.Module):
+
+    def __init__(self, lambda_param=0.1):
+        super(DiceDAResNet, self).__init__()
+
+        self.model = torchvision.models.resnet34(pretrained=True)
+        input_fc_dim = self.model.fc.in_features
+        self.model.fc = nn.Linear(input_fc_dim, 6)
+
+        self.domainfc = nn.Linear(input_fc_dim, 2)
+        self.ll = LambdaLayer(lambda_param=lambda_param)
+
+    def forward(self, x):
+        output = self.model.conv1(x)
+        output = self.model.bn1(output)
+        output = self.model.relu(output)
+        output = self.model.maxpool(output)
+
+        output = self.model.layer1(output)
+        output = self.model.layer2(output)
+        output = self.model.layer3(output)
+        output = self.model.layer4(output)
+
+        output = self.model.avgpool(output)
+        output = output.view(output.size(0), -1)
+
+        classif_output = self.model.fc(output)
+
         domain_output = ReverseLayerF.apply(output)
         domain_output = self.domainfc(domain_output)
         domain_output = self.ll(domain_output)
@@ -129,7 +123,6 @@ class Food101DAResNet(nn.Module):
         input_fc_dim = self.model.fc.in_features
         self.model.fc = nn.Linear(input_fc_dim, 101)
 
-        #self.grl = GradientReversalLayer()
         self.domainfc = nn.Linear(input_fc_dim, 2)
         self.ll = LambdaLayer(lambda_param=lambda_param)
 
@@ -149,7 +142,6 @@ class Food101DAResNet(nn.Module):
 
         classif_output = self.model.fc(output)
 
-        #domain_output = self.grl(output)
         domain_output = ReverseLayerF.apply(output)
         domain_output = self.domainfc(domain_output)
         domain_output = self.ll(domain_output)